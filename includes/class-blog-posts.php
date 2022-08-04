1c98ae1a170040acfd1fa8b2c54c4e7c35f255cf
Tanzeel-Ahmed <tanzeelahmed826@gmail.com>
2022-08-04 12:45:35 +0500

    remove CPT

--- includes/class-blog-posts.php
+++ includes/class-blog-posts.php
@@ -156,8 +156,6 @@ class Blog_Posts {
 
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
-		// Blog Posts
-		$this->loader->add_action( 'init', $plugin_admin, 'blog_post_type');
 
 	}
 
